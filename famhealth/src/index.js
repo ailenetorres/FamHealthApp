2ae9e3db48acd77da9a06cfc5abeefc281344601
dprado981 <46613080+dprado981@users.noreply.github.com>
2019-03-30 15:20:43 -0400

    a

--- famhealth/src/index.js
+++ famhealth/src/index.js
@@ -2,5 +2,5 @@ import React from "react"; //importing React Library
 import ReactDOM from "react-dom";
 import App from "./App"; //import main app component that wraps around everything
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(<App />, document.getElementById("meds"));
 //grabs the div from index.html and inserts <App />
