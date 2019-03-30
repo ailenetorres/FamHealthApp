7447965f4272dc914eeac538434d9257d17b2100
dprado981 <46613080+dprado981@users.noreply.github.com>
2019-03-30 17:26:27 -0400

    WOOOO MAPS

--- famhealth/src/index.js
+++ famhealth/src/index.js
@@ -1,9 +1,8 @@
 import React from "react"; //importing React Library
 import ReactDOM from "react-dom";
 import App from "./App"; //import main app component that wraps around everything
-import MapContainer from "./MapContainer";
+
 
 ReactDOM.render(<App />, document.getElementById("meds"));
-ReactDOM.render(<center><MapContainer /></center>, document.getElementById("maps"));
 
 //grabs the div from index.html and inserts <App />
