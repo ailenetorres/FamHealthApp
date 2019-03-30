2aecb51dcb20696fc6dd3c390e533aa48ce4cbfb
dprado981 <46613080+dprado981@users.noreply.github.com>
2019-03-30 12:56:24 -0400

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,16 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Bye World!"
+    user = {'username': 'Miguel'}
+    return '''
+<html>
+    <head>
+        <title>Home Page - Microblog</title>
+    </head>
+    <body>
+        <h1>Hello, ''' + user['username'] + '''!</h1>
+    </body>
+</html>'''
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0")
